a700124c2b3721faa220f59ec08f9ddb02dfb38c
YUVRAJ <yuvraj141104@gmail.com>
2026-08-24 23:03:01 +0530

    Perf: Re-enabled real-time FPS profiler telemetry HUD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,7 +169,7 @@ export default function Home() {
   return (
     <>
       <CustomCursor />
-      <FPSProfiler activeSection={activeSection} enabled={false} />
+      <FPSProfiler activeSection={activeSection} enabled={true} />
       {/* Intro Loader screen overlay */}
       {showIntro && (
         <IntroLoader 
